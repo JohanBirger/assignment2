7f6b249555edf49a3256e5e6aa5af0d07b35a2c8
JohanBirger <67735706+JohanBirger@users.noreply.github.com>
2020-07-19 12:50:05 +0200

    Update Assignment3.java

--- Assignment3.java
+++ Assignment3.java
@@ -1,17 +1,28 @@
+package assignment3;
+
 import java.util.Random;
 import java.util.Scanner;
 
-/**
- * A game of rock-paper-scissor
- * created by Teacher
- * commented by Johan Birgersson
+/** A game of rock paper scissor
+ * @author teacher
+ * Commented by Johan Birgersson
  */
+
 public class Assignment3 {
-    // Creates environment
+    /** Executes program
+     * @param args not used
+     */
     public static void main(String[] args) {
         run();
     }
-    // executions
+
+    /** run method initiates a scanner method to interpret user input.
+     *  Adds objects to be assigned value and creates array with elements to be used as markers for values in game. <br>
+     *  Whole game is encased in a while loop that constantly checks for a winner or termination, ergo a score value that triggers the <br>
+     *  iswinner variable to be true or user input that triggers quit boolean to be true. <br>
+     *  If user input is valid, ergo one of the elements in the alternatives array, the computer tries this value against a randomly generated element. <br>
+     */
+
     public static void run() {
         Scanner scanner = new Scanner(System.in);
         String inputString = "";
@@ -19,36 +30,36 @@ public class Assignment3 {
         String[] alternatives = {"rock", "scissors", "paper"};
         String[] methods = {"crushes", "cuts", "covers"};
         boolean quit = false, isWinner;
-        // While the game has not been terminated, computer asks for input.
+        // while the game has not been terminated, computer asks for input.
         while(!quit) {
             user_interface();
             isWinner = false;
             score = new int[2];
-            // While the game has no winner and has not been terminated, computer asks for input.
+            // while game has no winner and has not been terminated, computer asks for input.
             while(!quit && !isWinner) {
                 System.out.println("Make your choice (rock, paper or scissors): ");
                 inputString = scanner.nextLine().trim();
-                //if user input is q quit the program is terminated 
+                // if user input is "q" the program is terminated
                 if(inputString.equalsIgnoreCase("Q")) {
                     System.out.println("Bye!");
                     quit = true;
                 }
-                //handles user input to proceed game, initialising the input as empty.
+                // handles user input to proceed game, initialising the input as  empty.
                 else {
                     int input = -1;
                     // when the input is equal to one of the set alternatives that input is the users choice.
-                    for(int choice_from_alternatives = 0; choice_from_alternatives < alternatives.length; choice_from_alternatives ++) {
-                        if(alternatives[choice_from_alternatives].equalsIgnoreCase(inputString)) {
-                            input = choice_from_alternatives;
+                    for(int i = 0; i < alternatives.length; i ++) {
+                        if(alternatives[i].equalsIgnoreCase(inputString)) {
+                            input = i;
                         }
                     }
-                    //if the user does not provide input, comp tells user to choose a valid input.
+                    // if the user does not provide input, comp tells user to choose a valid input.
                     if(input == -1) {
                         System.out.println("Invalid input. Valid input is rock, paper or scissors.");
                     }
-
+                    //
                     else {
-                        //initialising a choice from the computer using the method random with a max of index 3.
+                        // initialising a choice from the computer using the method random with a max if index 3.
                         int computerChoice = new Random().nextInt(3);
                         System.out.println("Computer chooses " + alternatives[computerChoice] + ".");
                         // if computers choice wins the score increases for the computer.
@@ -57,13 +68,13 @@ public class Assignment3 {
                             System.out.println("Your " + alternatives[input] + " " + methods[input] + " computers " +
                                     alternatives[computerChoice] + " (" + score[0] + " - " + score[1] + ").");
                         }
-                        //if the computer choice equals the input, no score is recorded and game proceeds.
+                        //if the computer choice equals the input no score is recorded and the game proceeds.
                         else if(computerChoice == input) {
                             System.out.println("Same choise, no winner.");
                         }
 
                         else {
-                            //if user input wins the score is increased for the user
+                            // if user input wins the score is increased for the user.
                             score[1] ++;
                             System.out.println("Computers " + alternatives[computerChoice] + " " + methods[computerChoice] +
                                     " your " + alternatives[input] + " (" + score[0] + " - " + score[1] + ").");
@@ -75,18 +86,23 @@ public class Assignment3 {
         }
         scanner.close();
     }
-
+    // create method for user interface
     public static void user_interface() {
         System.out.println("** WELCOME TO ROCK-PAPER-SCISSORS **");
         System.out.println("The game where rock crushes scissors, scissors cuts paper and paper covers rock. Best of five wins.");
         System.out.println("Terminate program by typing Q.");
     }
 
+    /** creates method for score keeping, checks for winner
+     * @param player variable that holds player score
+     * @return score_limit is only used if "if statement" is true
+     */
+
     public static boolean score_limit(int[] player) {
-        //if
+        // someone has score of 3, check if user, if not computer wins.
         if(player[0] == 3 || player[1] == 3) {
             String winner = "Computer";
-
+            // if user has score of 3, user is winner.
             if(player[0] == 3) {
                 winner = "You";
             }
